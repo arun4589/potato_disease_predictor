e143ab44d63b31359f0803f5a45111d5732ebb85
Arun <erarunkumawat50@gmail.com>
2024-10-23 00:54:53 +0530

    callbacks added

--- src/Potato_disease_predictor_/config/configuration.py
+++ src/Potato_disease_predictor_/config/configuration.py
@@ -1,8 +1,8 @@
 from Potato_disease_predictor_.constants import *
 from Potato_disease_predictor_.utils.common import read_yaml,create_directories
 from Potato_disease_predictor_.entity.config_entity import (DataIngestionConfig,
-                                                            PrepareBaseModelConfig)
-
+                                                            PrepareBaseModelConfig,PrepareCallbacksConfig)
+import os
 class ConfigurationManager:
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
@@ -45,6 +45,24 @@ class ConfigurationManager:
         )    
 
         return prepare_base_model_config
+    
+    def get_prepare_callback_config(self)->PrepareCallbacksConfig:
+        config=self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])    
+
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
 
 
     
