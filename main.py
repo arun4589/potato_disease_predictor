90321c61bd34f71b7b675b3eb9c78c4046c6ee2f
Arun <erarunkumawat50@gmail.com>
2024-10-23 00:09:16 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Potato_disease_predictor_ import logger
 from Potato_disease_predictor_.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from Potato_disease_predictor_.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -10,4 +10,19 @@ try:
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===========x")
 except Exception as e:
-    raise e    
+    raise e  
+
+
+
+STAGE_NAME="Prepare base MOdel"
+
+try:
+        logger.info(f"******************")
+        logger.info(f">>>>>>stage {STAGE_NAME} started <<<<<<<")
+        prepare_base_model=PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
+        logger.info(f">>>>stage {STAGE_NAME} completed <<<<<<<\n\nx============x")
+
+
+except Exception as e:
+    raise e
