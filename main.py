eb7735ed5cb8d85b78c2274cba67c282cdfd18e6
Arun <erarunkumawat50@gmail.com>
2024-10-23 22:15:37 +0530

    training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Potato_disease_predictor_ import logger
 from Potato_disease_predictor_.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Potato_disease_predictor_.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from Potato_disease_predictor_.pipeline.stage_03_training import ModelTrainingPipeline 
 STAGE_NAME="Data Ingestion stage"
 
 try:
@@ -23,6 +23,21 @@ try:
         prepare_base_model.main()
         logger.info(f">>>>stage {STAGE_NAME} completed <<<<<<<\n\nx============x")
 
+
+except Exception as e:
+    raise e
+
+
+
+STAGE_NAME="Training"
+
+
+try:
+        logger.info(f"*****************")
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+        model_trainer=ModelTrainingPipeline()
+        model_trainer.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME}  completed <<<<<<<<\n\nx===========x")
 
 except Exception as e:
     raise e
